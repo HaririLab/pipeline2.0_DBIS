ab8ae3f6fdc5df3b5e683ef6d43a836cc47f5e88
annchenruth <aknodt@gmail.com>
2018-02-18 17:30:54 -0500

    sending output to TMPDIR

--- spm_fieldmap.m
+++ spm_fieldmap.m
@@ -19,7 +19,7 @@
 spmPath = '/mnt/BIAC/munin4.dhe.duke.edu/Hariri/DNS.01/Scripts/Tools/spm12'; addpath(genpath(spmPath));
 
 %Here we set some directory variables to make navigation easier
-homedir='/mnt/BIAC/munin4.dhe.duke.edu/Hariri/DBIS.01/Analysis/All_Imaging/'; 
+homedir='SUB_TMPOUTDIR_SUB'; 
 datadir='/mnt/BIAC/munin4.dhe.duke.edu/Hariri/DBIS.01/Data/OTAGO/';
 
 % spm('defaults','fmri');spm_jobman('initcfg');                               % Initialize SPM JOBMAN
@@ -32,7 +32,7 @@ if(length(images)>0)
     for j=1:numimages; imagearray{j}=[datadir '/SUB_SUBJECT_SUB/DMHDS/MR_gre_field_mapping_2mm/' images(j).name]; end;
     matlabbatch{1}.spm.util.dicom.data = imagearray;
     matlabbatch{1}.spm.util.dicom.root = 'flat';
-    matlabbatch{1}.spm.util.dicom.outdir = {[homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB']};
+    matlabbatch{1}.spm.util.dicom.outdir = {[homedir]};
     matlabbatch{1}.spm.util.dicom.convopts.format = 'nii';
     matlabbatch{1}.spm.util.dicom.convopts.icedims = 0;
     clear imagearray;
@@ -42,23 +42,23 @@ if(length(images)>0)
     % there should be 3 sDMHDS*.nii files!  typically 0006...01 & 02.nii and 0007...02.nii
     % the first 2 are magnitude images, and the first should have the shorter echo
     % the third is the phase difference image, calculated by the scanner
-    images=dir([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/s*nii']);
+    images=dir([homedir '/s*nii']);
     fprintf('***Renaming %s to magnitude_image_1.nii***\n',images(1).name);
-    movefile([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/' images(1).name],[homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/magnitude_image_1.nii']);
+    movefile([homedir '/' images(1).name],[homedir '/magnitude_image_1.nii']);
     fprintf('***Deleting magnitude_image_2 file %s***\n',images(2).name);
-    delete([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/' images(2).name]);
+    delete([homedir '/' images(2).name]);
     fprintf('***Renaming %s to phase_dif.nii***\n',images(3).name);
-    movefile([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/' images(3).name],[homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/phase_dif.nii']);
+    movefile([homedir '/' images(3).name],[homedir '/phase_dif.nii']);
 else
     fprintf('****Couldnt find fieldmap!!!****\n\n');
 end
 
 %% CALCULATE FIELDMAP
-gunzip([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/tmp/epi_dt.nii.gz']);
+gunzip([homedir '/tmp/epi_dt.nii.gz']);
 
-matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.phase = {[homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/phase_dif.nii,1']};
+matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.phase = {[homedir '/phase_dif.nii,1']};
 % use first magnitude images since it is the shorter echo and shoud have better contrast
-matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.magnitude = {[homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/magnitude_image_1.nii,1']};
+matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.magnitude = {[homedir '/magnitude_image_1.nii,1']};
 matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.defaults.defaultsval.et = [4.92 7.38];
 matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.defaults.defaultsval.maskbrain = 1;
 matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.defaults.defaultsval.blipdir = 1;  % ORIGINALLY USED -1
@@ -75,7 +75,7 @@ matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.defaults.defaultsval.mflag
 matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.defaults.defaultsval.mflags.ndilate = 4;
 matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.defaults.defaultsval.mflags.thresh = 0.5;
 matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.defaults.defaultsval.mflags.reg = 0.02;
-matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.session(1).epi = {[homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/tmp/epi_dt.nii,1']};
+matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.session(1).epi = {[homedir '/tmp/epi_dt.nii,1']};
 matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.matchvdm = 1;
 matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.sessname = 'session';
 matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj.writeunwarped = 0;
@@ -86,9 +86,9 @@ spm_jobman('run',matlabbatch);  clear matlabbatch     %Execute the job and clear
 
 %% APPLY FIELDMAP WITH REALIGN AND UNWARP
 % Get V000 images
-for j=1:SUB_NUMTRS_SUB; imagearray{j}=sprintf('%s/SUB_SUBJECT_SUB/SUB_TASK_SUB/tmp/epi_dt.nii,%d',homedir,j); end; 
+for j=1:SUB_NUMTRS_SUB; imagearray{j}=sprintf('%s/tmp/epi_dt.nii,%d',homedir,j); end; 
 matlabbatch{1}.spm.spatial.realignunwarp.data.scans = imagearray;
-matlabbatch{1}.spm.spatial.realignunwarp.data.pmscan = {[homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/vdm5_scphase_dif.nii']};
+matlabbatch{1}.spm.spatial.realignunwarp.data.pmscan = {[homedir '/vdm5_scphase_dif.nii']};
 matlabbatch{1}.spm.spatial.realignunwarp.eoptions.quality = 0.9;
 matlabbatch{1}.spm.spatial.realignunwarp.eoptions.sep = 4;
 matlabbatch{1}.spm.spatial.realignunwarp.eoptions.fwhm = 5;
@@ -115,7 +115,7 @@ matlabbatch{1}.spm.spatial.realignunwarp.uwroptions.prefix = 'u';
 spm_jobman('run',matlabbatch);  clear matlabbatch     %Execute the job and clear matlabbatch
 
 %% Convert motion parameters, and get FD 
-s_file = [homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/tmp/rp_epi_dt.txt']
+s_file = [homedir '/tmp/rp_epi_dt.txt']
 if exist(s_file,'file')
     M=dlmread(s_file);
 	rot_deg=zeros(size(M,1),3); % rotation in degrees
@@ -132,20 +132,20 @@ if exist(s_file,'file')
         end
         FD(i)=abs(M(i-1,1)-M(i,1))+abs(M(i-1,2)-M(i,2))+abs(M(i-1,3)-M(i,3))+abs(rot_mm(i-1,1)-rot_mm(i,1))+abs(rot_mm(i-1,2)-rot_mm(i,2))+abs(rot_mm(i-1,3)-rot_mm(i,3));
     end
-    dlmwrite([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/motion_spm_rad.1D'],M,'delimiter',' ')
-    dlmwrite([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/motion_spm_deg.1D'],[M(:,1:3) rot_deg],'delimiter',' ')
-    dlmwrite([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/FD.1D'],FD,'delimiter',' ')
+    dlmwrite([homedir '/motion_spm_rad.1D'],M,'delimiter',' ')
+    dlmwrite([homedir '/motion_spm_deg.1D'],[M(:,1:3) rot_deg],'delimiter',' ')
+    dlmwrite([homedir '/FD.1D'],FD,'delimiter',' ')
 else
     fprintf('****Error: %s does NOT exist.*****\n', s_file);
 end
 
 %% clean up
-gzip([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/vdm5_scphase_dif.nii']);
-delete([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/vdm5_scphase_dif.nii']);
-delete([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/magnitude_image_1.nii']); 
-delete([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/phase_dif.nii']); 
-delete([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/fpm_scphase_dif.nii']);
-delete([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/bmaskmagnitude_image_1.nii']);
-delete([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/mmagnitude_image_1.nii']);
-delete([homedir '/SUB_SUBJECT_SUB/SUB_TASK_SUB/scphase_dif.nii']);
+gzip([homedir '/vdm5_scphase_dif.nii']);
+delete([homedir '/vdm5_scphase_dif.nii']);
+delete([homedir '/magnitude_image_1.nii']); 
+delete([homedir '/phase_dif.nii']); 
+delete([homedir '/fpm_scphase_dif.nii']);
+delete([homedir '/bmaskmagnitude_image_1.nii']);
+delete([homedir '/mmagnitude_image_1.nii']);
+delete([homedir '/scphase_dif.nii']);
 
